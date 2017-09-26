d1107650cd04138bba5a7b6c407cee513418a85b
Antti Leinonen <K9373@student.jamk.fi>
2017-09-26 21:45:43 +0300

    Tehtävä2 valmis

---
+++ Labra4Tehtä2/Labra4Tehtä2/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra4Tehtä2
+{
+    class Elevator
+    {
+        
+        private const int minFloor = 1;
+        private const int maxFloor = 5;
+        private int floor = 1;
+
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value < minFloor || value > maxFloor)
+                
+                {
+                    Console.WriteLine("Ei ole tuollaista kerrosta, mistä löysit tuon kerroksen nappulan?");
+                }
+                        
+                else
+                {
+                    floor = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Labra4Tehtä2/Labra4Tehtä2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra4Tehtä2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Elevator elevator = new Elevator();
+            bool result;
+            
+                Console.WriteLine("Hissi on nyt kerroksessa : " + elevator.Floor);
+                Console.WriteLine("Anna kerroksen numero (1-5) > ");
+
+                String line = Console.ReadLine();
+                int floor;
+                result = Int32.TryParse(line, out floor);
+
+                if (result)
+                {
+                    elevator.Floor = floor;
+                }
+
+            Console.WriteLine("Hissi on nyt kerroksessa " + floor);
+
+
+
+
+
+
+
+
+        }
+    }
+}
