7508d09d4c2dbb487072b840d9ad9868de7d7ee2
Antti Leinonen <K9373@student.jamk.fi>
2017-10-10 18:40:50 +0300

    asf

---
+++ Labra6Tehtä2/Labra6Tehtä2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra6Tehtä2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Jaakaappi oma = new Jaakaappi("Samsung ", "RB28h ", 24);
+            Console.WriteLine("Onneksi olkoon uudesta jääkaapista {0}", oma);
+        }
+
+        
+
+        class Jaakaappi
+        {
+            public string Valmistaja { get; set; }
+            public string Malli { get; set; }
+            public int Kaljat { get; set; }
+
+            public Jaakaappi(string valmistaja, string malli, int kaljat)
+            {
+                Valmistaja = valmistaja;
+                Malli = malli;
+                Kaljat = kaljat;
+            }
+
+            public override string ToString()
+            {
+                return "Valmistaja: " + Valmistaja + "Malli: " + Malli + "Sisällä: " + Kaljat + " olutta.";
+            }
+
+
+
+
+
+
+        }
+            
+    }
+}
+
+
