fbd7cf77f6a395d3aed3b1cfef6807bc61270ec5
Antti Leinonen <K9373@student.jamk.fi>
2017-10-10 20:22:08 +0300

    asfasfafs

---
+++ Labra6Tehtä3/Labra6Tehtä3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra6Tehtä3
+{
+    class Program
+    { 
+
+     static void Main(string[] args)
+     {
+
+         Console.WriteLine("Seuran pelaajat:");
+
+         List<Pelaaja> Pelaajat = new List<Pelaaja>();
+            foreach (Pelaaja item in Pelaajat)
+            {
+            Console.WriteLine(item.ToString());
+            }
+                 
+           
+
+           Seura jyp = new Seura();
+             jyp.Nimi = "JYP";
+             jyp.Pelaajat.Add(new Pelaaja("Timo", "Taikuri", "P", "L"));
+           Console.WriteLine(jyp.ToString());
+
+        }
+    }
+
+}
