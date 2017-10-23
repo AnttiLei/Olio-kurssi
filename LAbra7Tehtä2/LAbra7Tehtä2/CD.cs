08073a095155de3358adbd48dd0aa5345b06ada1
Antti Leinonen <K9373@student.jamk.fi>
2017-10-23 19:19:11 +0300

    tehtävä2 done

---
+++ LAbra7Tehtä2/LAbra7Tehtä2/CD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAbra7Tehtä2
+{
+    class Cd
+    {
+        #region PROPERTIES
+        public string Nimi { get; set; }
+        public string Artist { get; set; }
+        public string Songs { get; set; }
+        
+        #endregion
+        #region CONSTRUCTORS
+        public Cd()
+        { }
+        public Cd(string nimi, string artist, string songs)
+        {
+            Nimi = nimi;
+            Artist = artist;
+            Songs = songs;
+            
+        }
+       
+        #endregion
+        #region METHODS
+        public override string ToString()
+        {
+            return string.Format("Levyn nimi: {0} Artisti: {1} Kappaleet: {2}", Nimi, Artist, Songs);
+        }
+        #endregion
+    }
+}
---
+++ LAbra7Tehtä2/LAbra7Tehtä2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAbra7Tehtä2
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            //Luodaan List-tyyppinen lista levyistä
+            List<Cd> levy = new List<Cd>();
+            
+            levy.Add(new Cd("Endless Forms Most Beatiful", "Nightwish", "Kappale1, 4:40 kappale2 4:40, kappale3 4:40"));
+            levy.Add(new Cd("Tosi hyvä levy", "Paikallinen bändi", "Kappale1, 2:40 kappale2 2:40, kappale3 2:40"));
+            levy.Add(new Cd("Toisiks paras levy", "Paikallinen bändi", "Kappale1, 3:40 kappale2 3:40, kappale3 3:40"));
+
+
+            //kaikkien levyjen tiedot
+            Console.WriteLine("Jukeboksin levyt");
+            foreach (var o in levy)
+            {
+                Console.WriteLine(o.ToString());
+            }
+
+        }
+    }
+    
+}
