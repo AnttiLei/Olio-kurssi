778cf4ec06aeef87194a83fe56fcdb6affc15a92
Antti Leinonen <K9373@student.jamk.fi>
2017-10-23 20:51:21 +0300

    tehtä3done

---
+++ Labra7Tehtä3/Labra7Tehtä3/Deck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra7Tehtä3
+{
+    class Deck
+    {
+        public string Kortti { get; set; }
+
+        public Deck(string kortti)
+        {
+            Kortti = kortti;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}", Kortti);
+        }
+    }
+}
