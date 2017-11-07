dc4a56e17078eaaecae805b127e8bf3da3067901
Antti Leinonen <K9373@student.jamk.fi>
2017-11-07 20:50:19 +0200

    labra9teht2

---
+++ Labra9teht2/Labra9teht2/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra9teht2
+{
+    class Product
+    {
+        public string Ostoskori { get; set; }
+        public double Hinta { get; set; }
+
+        public Product(string ostoskori, double hinta)
+        {
+            Ostoskori = ostoskori;
+            Hinta = hinta;
+        }
+                    
+        public override string ToString()
+        {
+            return "Tuote : " + Ostoskori + " " + Hinta + "euroa";
+               
+        }
+    }
+}
+
+
---
+++ Labra9teht2/Labra9teht2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra9teht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> kori = new List<Product>();
+            kori.Add(new Product("Maito", 1.4));
+            kori.Add(new Product("Olvi kolomonen", 2.2));
+            kori.Add(new Product("Voi", 3.2));
+            kori.Add(new Product("Juusto", 4.2));
+
+            Console.WriteLine("Ostoskori sisältää:");
+
+            foreach (var o in kori)
+            {
+                Console.WriteLine(o.ToString());
+                
+            }
+        }
+
+    }
+}
