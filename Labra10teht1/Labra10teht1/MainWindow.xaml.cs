1db9e1e4992422c00967ab28a91d73914a90d84d
Antti Leinonen <K9373@student.jamk.fi>
2017-11-13 21:38:01 +0200

    Labra10teht1 done

---
+++ Labra10teht1/Labra10teht1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra10teht1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        int cars = 0;
+        int trucks = 0;
+        private void Trucks_Click(object sender, RoutedEventArgs e)
+        {
+            trucks++;
+            InfoTrucks.Text = trucks.ToString();
+        }
+
+        private void Cars_Click(object sender, RoutedEventArgs e)
+        {
+            cars++;
+            InfoCars.Text = cars.ToString();
+        }
+    }
+}
