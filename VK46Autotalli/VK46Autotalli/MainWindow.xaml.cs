6cfb31a9f4cc6c1741f312014705973094343e8e
k9373 <k9373@ELEK-436-WS-28>
2017-11-15 11:03:53 +0200

    Tuntiharjoitus done.

---
+++ VK46Autotalli/VK46Autotalli/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.IT;
+
+namespace VK46Autotalli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Auto> autot; //muuttuja on käytettävissä kaikissa luokas metodeissa ja tapahtumankäsittelijöissä
+        private const string polku = @"d:\\kuvat\\";
+        public MainWindow()
+        {
+            //Tänne koodi joka suoritetaan ikkunan avauksen luonnissa
+            InitializeComponent();
+            //Pyydetään BL-kerrokselta autot, ja näytetään ne käyttäjälle
+            autot = Autotalli.HaeAutot();
+            NaytaKuva("autotalli.png");
+            //Asetetaan comboboxiin kaikki eri automerkit
+            //Vaihtoehto 1: Käsin koodaamalla
+            List<string> merkit = new List<string>();
+            merkit.Add("Audi");
+            merkit.Add("Saab");
+            merkit.Add("Volvo");
+            merkit.Add("Renault");
+            //Vaihtoehto 2: Parempi, kysytään LINQ:lla datasta eri automerkit
+            var result = autot.Select(m => m.Merkki).Distinct();
+            cmbAutot.ItemsSource = result;
+        }
+
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {
+            dgAutot.ItemsSource = autot;
+
+        }
+        private void NaytaKuva(string url)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = "puuttuu.png";
+                }
+                //Lisätään kuvatiedostojen vakiopolku
+                url = polku + url;
+                //Kuvan näyttö
+                BitmapImage pic = new BitmapImage();
+                pic.BeginInit();
+                pic.UriSource = new Uri(url);
+                pic.EndInit();
+                imgAuto.Stretch = Stretch.Fill;
+                imgAuto.Source = pic;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                throw;
+            }
+        }
+
+        private void dgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //huom, olemme itse populoineet DataGridin Auto-olioilla, joten kukin DataGridin jäsen/alkio on itseasiassa Auto-olio
+            object obj = dgAutot.SelectedItem;
+            if (obj != null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+        }
+
+        private void btnHaeAudit_Click(object sender, RoutedEventArgs e)
+        {
+            //Näkyviin pelkästään Audi-merkkiset autot
+            var result = autot.Where(m => m.Merkki.Contains("Audi"));
+            dgAutot.ItemsSource = result;
+        }
+
+        private void cmbAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Suodatetaan DataGridiin näkyviin vain valittu automerkki
+            string merkki = cmbAutot.SelectedValue.ToString();
+            var result = autot.Where(m => m.Merkki.Contains(merkki));
+            dgAutot.ItemsSource = result;
+            
+        }
+    }
+}
---
+++ VKO44Tunti/VKO44Tunti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO44Tunti
+{
+    interface Icalculator
+
+    public class MyCalculator : Icalculator
+    {
+        public int Add(int number1, int number2);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCalculator laskin = new MyCalculator();
+            int luku1 = 10;
+            int luku2 = 20;
+            Console.WriteLine("Lukujen {0} ja {1} summa on {2}", luku1, luku2, laskin.Add)
+        }
+    }
+}
