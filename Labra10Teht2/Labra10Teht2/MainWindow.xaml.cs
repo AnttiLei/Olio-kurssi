dab70fe58929ef9e56eedc50c8054ae8196c5806
Antti Leinonen <K9373@student.jamk.fi>
2017-11-21 18:56:32 +0200

    Labra11Teht1 done.

---
+++ Labra10Teht2/Labra10Teht2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra10Teht2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                double maksusumma;
+                double muunnettuosto;
+                if (double.TryParse(txtOstettavasumma.Text, out muunnettuosto))
+                {
+                    if (cmbValuutat.Text == "Yhdysvallat USD")
+                    {
+                        maksusumma = muunnettuosto * 0.8997;
+                        string muunnettumaksu = maksusumma.ToString("0.00");
+                        txbMaksettavasumma.Text = muunnettumaksu;
+                        txbMaksuvaluutta.Text = "Suomi, Eur";
+                    }
+
+                    else if (cmbValuutat.Text == "Suomi EUR")
+                    {
+                        maksusumma = muunnettuosto / 0.8997;
+                        string muunnettumaksu = maksusumma.ToString("0.00");
+                        txbMaksettavasumma.Text = muunnettumaksu;
+                        txbMaksuvaluutta.Text = "Yhdysvallat, USD";
+                    }
+                }
+                else
+                {
+                    txtOstettavasumma.Text = "0";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Labra11Teht1/Labra11Teht1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra11Teht1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        //Stack<string> stackStrings = new Stack<string>();
+
+        private void Milk_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Milk.IsChecked)
+            {
+                
+                List.Text = List.Text + "Milk\n";
+            }
+           
+        }
+
+        private void Butter_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Butter.IsChecked)
+            {
+                List.Text = List.Text + "Butter\n";
+            }
+        }
+
+        private void Beer_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Beer.IsChecked)
+            {
+                List.Text = List.Text + "Beer\n";
+            }
+        }
+
+        private void Chicken_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Chicken.IsChecked)
+            {
+                List.Text = List.Text + "Chicken\n";
+            }
+        }
+
+        private void Lemonade_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Lemonade.IsChecked)
+            {
+                List.Text = List.Text + "Lemonade\n";
+            }
+        }
+
+        private void List_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Buy_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
